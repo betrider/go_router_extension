bd54385be870de363d486f22f042529731f3f83f
betrider <tlsfpem@gmail.com>
2023-08-31 16:56:03 +0900

    필요없는 파일 제거

--- lib/go_router_extension.dart
+++ lib/go_router_extension.dart
@@ -1,12 +1,3 @@
-
-import 'go_router_extension_platform_interface.dart';
-
-class GoRouterExtension {
-  Future<String?> getPlatformVersion() {
-    return GoRouterExtensionPlatform.instance.getPlatformVersion();
-  }
-}
-
 class ClassNameAnnotation {
   final Type className;
   const ClassNameAnnotation({required this.className});
